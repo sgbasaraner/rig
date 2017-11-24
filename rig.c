871182a1971a564a90f46da373418febfb28396a
Sarp Basaraner <sarpbasaraner@gmail.com>
2017-11-24 11:25:13 +0300

    convert user input

--- rig.c
+++ rig.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
 	if (argc != 3){
@@ -7,4 +9,13 @@ int main(int argc, char *argv[]) {
 	}
 
 	printf("W: %s, H: %s\n", argv[1], argv[2]);
+
+	// remember width and height
+	int16_t width = atoi(argv[1]);
+	int16_t height = atoi(argv[2]);
+
+	if ((width == 0) || (height == 0)) {
+		printf("Invalid entry\n");
+		return 2;
+	}
 }
